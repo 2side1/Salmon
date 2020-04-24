d6996d70b25c135d226aad0e1452789232167b73
woodandmortar <moreforblack@gmail.com>
2020-04-24 14:20:04 -0500

    game

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
-const server = require('express')();
-const http = require('http').createServer(server);
-const io = require('socket.io')(http);
-let players = [];
+// This file starts the both the Express server, used to serve the actual webpage,
+// and the Socket.io server, used to handle the the realtime connection to the client.
 
-io.on('connection', function (socket) {
-    console.log('A user connected: ' + socket.id);
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("./libs/game_manager").listen(http);  // Start Socket.io server and let game_manager handle those connections
 
-    players.push(socket.id);
+app.set("port", (process.env.PORT || 3001));  // Use either given port or 3001 as default
+app.use(express.static("public"));  // Staticly serve pages, using directory 'public' as root 
 
-    if (players.length === 1) {
-        io.emit('isPlayerA');
-    };
-
-    socket.on('dealCards', function () {
-        io.emit('dealCards');
-    });
-
-    socket.on('cardPlayed', function (gameObject, isPlayerA) {
-        io.emit('cardPlayed', gameObject, isPlayerA);
-    });
-
-    socket.on('disconnect', function () {
-        console.log('A user disconnected: ' + socket.id);
-        players = players.filter(player => player !== socket.id);
-    });
+// User connects to server
+app.get("/", function(req, res) {
+	// Will serve static pages, no need to handle requests
 });
 
-http.listen(3000, function () {
-    console.log('Server started!');
+// If any page not handled already handled (ie. doesn't exists)
+app.get("*", function(req, res) {
+	res.status(404).send("Error 404 - Page not found");
+});
+
+// Start http server
+http.listen(app.get("port"), function() {
+	console.log("Node app started on port %s", app.get("port"));
 });
